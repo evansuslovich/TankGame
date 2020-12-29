1a76cc096e317d9e91d302217fced48f246f5315
evansuslovich <evan.suslovich@lincolnps.org>
2020-12-29 18:44:01 -0500

    Set up frame and panel

--- Tank.java
+++ Tank.java
@@ -1,6 +1,21 @@
-public class Tank{
+import javax.swing.*; 
+import java.awt.*; 
+
+public class Tank extends JFrame {
+    private static final long serialVersionUID = 1L;
+
+    JPanel panel = new JPanel(); 
+
     public Tank(){
-        System.out.println("Tank"); 
+        setTitle("Tank Game"); 
+        setSize(400,400); 
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        panel.setBackground(Color.LIGHT_GRAY); 
+
+
+        add(panel); 
+        setVisible(true); 
     }
     public static void main(String[] args){
         new Tank(); 
